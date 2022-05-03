80f7dd76e7bd47ade5726892a4bd1e8fcf9dc5f3
Peter Pakanun <lpeachl@gmail.com>
2022-05-04 00:43:12 +0700

    install deps

---
+++ src/routes/import/index.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export async function load({ session }) {
+    if (!session?.user) {
+      return {
+        status: 302,
+        redirect: '/users/login?redirect=%2Fimport'
+      };
+    }
+    return {
+      props: {
+        user: session.user
+      }
+    };
+  }
+</script>
+
+<script>
+	import { read, utils } from 'xlsx';
+	import Button from '$lib/components/Button.svelte';
+	import FileInput from '$lib/components/FileInput.svelte';
+
+  // export let user;
+
+</script>
