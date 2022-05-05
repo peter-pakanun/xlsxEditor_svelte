9a59edeb4f0957f18c8a5b66edc71084d82f5333
Peter Pakanun <lpeachl@gmail.com>
2022-05-06 02:23:21 +0700

    shadow and color

--- src/lib/components/Block.svelte
+++ src/lib/components/Block.svelte
@@ -29,25 +29,29 @@
 		</div>
 
 		<div class="px-2 pt-1">
-			{#if block.tlNote}<div class="py-1 italic text-slate-500">Translation Note: {block.tlNote}</div>{/if}
+			{#if block.tlNote}
+				<div class="py-1 italic text-slate-500">
+					Translation Note: {block.tlNote}
+				</div>
+			{/if}
 
 			{#each fields as field}
 				{#if block.oStrs[field]}
-					<h2 class="mb-1">{field}</h2>
+					<h2 class="mb-1 text-sky-300/50">{field}</h2>
 					<div class="flex gap-1 text-sm">
 						<div class="relative flex-1">
 							<textarea
 								bind:value={block.oStrs[field]}
-								class="w-full h-20 p-1 rounded shadow-sm outline-none resize-none text-slate-400 bg-slate-900/25"
+								class="w-full h-20 p-1 rounded shadow-inner outline-none resize-none text-slate-400 bg-slate-900/25"
 								readonly
 							/>
-							<!-- <div class="absolute top-0 left-0 w-full h-20 p-1 text-orange-100 rounded shadow-sm outline-none pointer-events-none">{block.oStrs[field]}</div> -->
+							<!-- <div class="absolute top-0 left-0 w-full h-20 p-1 text-orange-100 rounded outline-none pointer-events-none">{block.oStrs[field]}</div> -->
 						</div>
 						<div class="flex-1">
 							<textarea
 								bind:value={block.tStrs[field]}
 								on:input={() => (editing = true)}
-								class="w-full h-20 p-1 rounded shadow-sm outline-none resize-none bg-slate-900/50 text-slate-200 {editing
+								class="w-full h-20 p-1 rounded outline-none resize-none shadow-inner bg-slate-900/50 text-slate-200 {editing
 									? 'ring-2 ring-purple-800 bg-purple-900/50'
 									: ''}"
 							/>
