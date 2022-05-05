df95ac4b4cce18c263924815c39f670b113cb842
Peter Pakanun <lpeachl@gmail.com>
2022-05-05 18:25:53 +0700

    editor with sheetlists

---
+++ src/routes/editor/index.js
@@ -0,0 +1,26 @@
+import clientPromise from '$lib/db';
+
+export async function get({ params, locals }) {
+  let { user } = locals;
+  if (!user) {
+    return {
+      body: {}
+    };
+  }
+
+  const client = await clientPromise;
+  const Definitions = client.db().collection('definitions');
+
+  let definition = await Definitions.find({ language: user.language }).sort({ version: -1 }).limit(1).toArray();
+  if (definition.length <= 0) {
+    return {
+      body: {}
+    };
+  }
+
+  return {
+    body: {
+      definition: definition[0]
+    }
+  }
+}
