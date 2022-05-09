59990590a3148ec669d544d34f3ae6f0377ec005
Peter Pakanun <lpeachl@gmail.com>
2022-05-09 14:06:47 +0700

    shortcut for searchbox

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -61,6 +61,7 @@
   }
 
   let query;
+  let searchRef;
   async function searchKeyDown(e) {
     if (e.key === 'Enter') {
       if (!query) return;
@@ -75,16 +76,26 @@
       }
     }
   }
+  async function docKeyDown(e) {
+    if (e.target !== searchRef && (e.ctrlKey || e.metaKey) && e.key === 'f') {
+      e.preventDefault();
+      e.stopPropagation();
+      searchRef.focus();
+    }
+  }
+
 </script>
 
 <svelte:head>
   <title>XLSX Editor</title>
 </svelte:head>
 
+<svelte:body on:keydown={docKeyDown}></svelte:body>
+
 <div class="px-6 py-2 text-white bg-indigo-900/50">
   <nav class="flex items-center justify-between h-12 px-4 mx-auto text-lg max-w-7xl">
     <div class="flex items-center gap-4">
-      <input type="text" class="h-8 px-3 rounded text-slate-200 bg-slate-900/50" placeholder="Search..." bind:value={query} on:keypress={searchKeyDown}>
+      <input type="text" class="h-8 px-3 rounded text-slate-200 bg-slate-900/50" placeholder="Search..." bind:this={searchRef} bind:value={query} on:keypress={searchKeyDown}>
     </div>
     <div class="flex items-center">
       <div>
@@ -99,24 +110,24 @@
   <div>
     <div class="flex flex-col overflow-x-hidden overflow-y-auto rounded shadow-md bg-slate-800 max-h-96">
       {#if lv2Sheets.length}
-        <h1 class="px-2 text-sm text-white bg-red-500/75">New Entires</h1>
+        <h1 class="px-2 text-sm text-white bg-red-500">New Entires</h1>
         {#each lv2Sheets as sheet}
           <div class="text-slate-200">
             <input type="checkbox" class="hidden peer" bind:checked={sheet.active} />
             <div class="flex transition-all duration-75 border-b cursor-pointer border-slate-900 group peer-checked:bg-blue-500/75 hover:bg-blue-500/50">
-              <div class="w-1 mr-1 bg-red-500/75 group-hover:w-2 group-hover:mr-0 group-hover:transition-all group-hover:duration-75"></div>
+              <div class="w-1 mr-1 bg-red-500 group-hover:w-2 group-hover:mr-0 group-hover:transition-all group-hover:duration-75"></div>
               <div class="flex-1 px-4 py-2" on:click={setActiveSheet(sheet.name)}>{sheet.name}</div>
             </div>
           </div>
         {/each}
       {/if}
       {#if lv1Sheets.length}
-        <h1 class="px-2 text-sm text-white bg-orange-500/75">Changed Entires</h1>
+        <h1 class="px-2 text-sm text-white bg-orange-500">Changed Entires</h1>
         {#each lv1Sheets as sheet}
           <div class="text-slate-200">
             <input type="checkbox" class="hidden peer" bind:checked={sheet.active} />
             <div class="flex transition-all duration-75 border-b cursor-pointer border-slate-900 group peer-checked:bg-blue-500/75 hover:bg-blue-500/50">
-              <div class="w-1 mr-1 bg-orange-500/75 group-hover:w-2 group-hover:mr-0 group-hover:transition-all group-hover:duration-75"></div>
+              <div class="w-1 mr-1 bg-orange-500 group-hover:w-2 group-hover:mr-0 group-hover:transition-all group-hover:duration-75"></div>
               <div class="flex-1 px-4 py-2" on:click={setActiveSheet(sheet.name)}>{sheet.name}</div>
             </div>
           </div>
