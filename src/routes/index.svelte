fe58a5c80265462c1076951c3228fbca8f72680c
Peter Pakanun <lpeachl@gmail.com>
2022-05-10 02:12:20 +0700

    remove tabindex from page btn

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -110,11 +110,11 @@
     </div>
     <div class="flex items-center">
       <div class="flex items-center gap-3 mr-4">
-        <button class="p-1 transition rounded bg-slate-900/50 hover:bg-slate-600/50" title="ALT + Left Arrow" on:click={() => pageInc({detail: -1})}>
+        <button class="p-1 transition rounded bg-slate-900/50 hover:bg-slate-600/50" title="ALT + Left Arrow" on:click={() => pageInc({detail: -1})} tabindex="-1">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>
         </button>
         <span>Page {curPage + 1} of {curMaxPage}</span>
-        <button class="p-1 transition rounded bg-slate-900/50 hover:bg-slate-600/50" title="ALT + Right Arrow" on:click={() => pageInc({detail: 1})}>
+        <button class="p-1 transition rounded bg-slate-900/50 hover:bg-slate-600/50" title="ALT + Right Arrow" on:click={() => pageInc({detail: 1})} tabindex="-1">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
         </button>
       </div>
