d4635bcb356bfb306fc6d3ff9d64a0d6cdc53415
Peter Pakanun <lpeachl@gmail.com>
2022-05-09 21:38:42 +0700

    combine search and [sheet] into one endpoint

---
+++ src/routes/blocks/[sheet].js
@@ -0,0 +1,79 @@
+import clientPromise from '$lib/db';
+
+let pageSize = 20;
+
+export async function get({ params, locals, url }) {
+  let { user } = locals;
+  let { sheet } = params;
+  let query = url.searchParams?.get('q');
+  let page = url.searchParams?.get('page');
+
+  if (!user) {
+    return {
+      status: 401,
+    };
+  }
+  if (!user.language) {
+    return {
+      status: 403,
+    };
+  }
+  if (!sheet) {
+    return {
+      status: 400,
+    };
+  }
+  if (!query) {
+    return {
+      status: 400,
+    };
+  }
+  if (!page || Number.isNaN(page)) {
+    page = 0;
+  }
+
+  const client = await clientPromise;
+  const Blocks = client.db().collection(user.language + '_blocks');
+
+  let agg = [{
+    $search: {
+      index: 'default',
+      text: {
+        query,
+        path: {
+          wildcard: '*'
+        }
+      }
+    }
+  }];
+  if (sheet.toLowerCase() !== 'all') {
+    agg.push({ $match: { sheet } });
+  }
+  agg.push(...[
+    { $sort: { aLV: -1 } },
+    { $facet: {
+      total: [
+        { $count: 'sheet' }
+      ],
+      pages: [
+        { $skip: page * pageSize },
+        { $limit: pageSize }
+      ],
+    }},
+  ]);
+
+  let searchResults = await Blocks.aggregate(agg).toArray().catch((e) => { console.error(e); });
+
+  if (!Array.isArray(searchResults)) {
+    return {
+      status: 500,
+    };
+  }
+
+  return {
+    body: {
+      pages: searchResults[0].pages,
+      total: searchResults[0].total[0]?.sheet ?? 0,
+    }
+  }
+}
