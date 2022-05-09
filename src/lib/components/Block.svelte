26b25b00939a9c74a8bac77ee470c5ae626dd502
Peter Pakanun <lpeachl@gmail.com>
2022-05-10 00:43:20 +0700

    move textarea out of Block

--- src/lib/components/Block.svelte
+++ src/lib/components/Block.svelte
@@ -1,6 +1,5 @@
 <script>
-	import { createEventDispatcher, onMount } from 'svelte';
-	const dispatch = createEventDispatcher();
+	import BlockTextarea from '$lib/components/BlockTextarea.svelte';
 
 	export let definition = {
 		sheets: [
@@ -8,7 +7,7 @@
 				name: 'sheet_1',
 				fields: ['field_1'],
 				attentionLevel: 1
-			},
+			}
 		]
 	};
 	export let block = {
@@ -26,51 +25,16 @@
 	export let isFirstBlock = false;
 	export let isLastBlock = false;
 
-	let editorRef = [];
-
 	let fields = ['field_1'];
-	$: fields = definition.sheets.find(sheet => sheet.name === block.sheet).fields;
+	$: fields = definition.sheets.find((sheet) => sheet.name === block.sheet).fields;
 
 	let colorClass;
-	$: colorClass =
-		block.aLV >= 2 ? 'bg-red-500/75' : block.aLV >= 1 ? 'bg-orange-500/75' : 'bg-slate-700';
-	let editing = false;
+	$: colorClass = 
+		block.aLV >= 2 ? 'bg-red-500/75' : 
+		block.aLV >= 1 ? 'bg-orange-500/75' : 
+										 'bg-slate-700';
 
 	let expaned = false;
-	function focused(e) {
-		e.target.select();
-		expaned = true;
-	}
-	function blured() {
-		expaned = false;
-	}
-	function keydown(e, fieldId) {
-		let curFieldsLength = 0;
-		for (const fieldKey in block.oStrs) {
-			if (Object.hasOwnProperty.call(block.oStrs, fieldKey)) {
-				if (fields.includes(fieldKey)) {
-					curFieldsLength++;
-				}
-			}
-		}
-		
-		if (isFirstBlock && fieldId === 0 && e.key === 'Tab' && e.shiftKey) {
-			e.preventDefault();
-			e.stopPropagation();
-			dispatch('pageInc', -1);
-		}
-		if (isLastBlock && fieldId === curFieldsLength - 1 && e.key === 'Tab' && !e.shiftKey) {
-			e.preventDefault();
-			e.stopPropagation();
-			dispatch('pageInc', 1);
-		}
-	}
-	onMount(() => {
-		if (isFirstBlock) {
-			editorRef[0].focus();
-			editorRef[0].select();
-		}
-	});
 </script>
 
 <div class="flex overflow-hidden text-xs rounded-lg shadow bg-slate-800">
@@ -93,24 +57,31 @@
 					<h2 class="mb-1 text-sky-300/50">{field}</h2>
 					<div class="flex gap-1 text-sm">
 						<div class="relative flex flex-1 gap-2">
-							<input class="hidden peer" type="checkbox" name="" id="" tabindex="-1" bind:checked={expaned} />
+							<input
+								class="hidden peer"
+								type="checkbox"
+								name=""
+								id=""
+								tabindex="-1"
+								bind:checked={expaned}
+							/>
 							<textarea
 								bind:value={block.oStrs[field]}
-								class="w-full h-8 p-1 transition-all rounded shadow-inner outline-none resize-none peer-checked:h-36 text-slate-400 bg-inherit peer-checked:bg-slate-900/25"
+								class="w-full h-8 p-1 transition-all rounded shadow-inner outline-none resize-none peer-checked:h-36 bg-slate-900/25 text-slate-400"
 								readonly
 								tabindex="-1"
 							/>
 							<!-- <div class="absolute top-0 left-0 w-full h-20 p-1 text-orange-100 rounded outline-none pointer-events-none">{block.oStrs[field]}</div> -->
-							<textarea
-								bind:this={editorRef[fieldId]}
+							<BlockTextarea
 								bind:value={block.tStrs[field]}
-								on:input={() => (editing = true)}
-								on:focus={focused}
-								on:blur={blured}
-								on:keydown={(e) => keydown(e, fieldId)}
-								class="w-full h-8 p-1 ring-offset-1 ring-offset-transparent peer-checked:h-36 transition-all rounded outline-none resize-none shadow-inner peer-checked:bg-slate-900/50 text-slate-200 {editing
-									? 'ring-2 ring-purple-800 bg-purple-900/50'
-									: 'focus:ring-sky-500 focus:ring-2 bg-slate-900/25'}"
+								bind:expaned={expaned}
+								{fields}
+								{fieldId}
+								{block}
+								{isFirstBlock}
+								{isLastBlock}
+								strType="tStrs"
+								on:pageInc
 							/>
 						</div>
 					</div>
---
+++ src/lib/components/BlockTextarea.svelte
@@ -0,0 +1,81 @@
+<script>
+	import { createEventDispatcher, onMount } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+  export let value = "";
+  let decodedValue = "";
+  $: {
+    decodedValue = value.replaceAll('\\n', '\n');
+  }
+  function decodedValueChanged(e) {
+    value = decodedValue.replaceAll("\n", "\\n");
+  }
+
+  export let fields = [];
+  export let fieldId = 0;
+  export let isFirstBlock = false;
+  export let isLastBlock = false;
+
+  export let block = {};
+  let curFieldsLength = 0;
+  $: {
+    curFieldsLength = 0;
+    for (const fieldKey in block.oStrs) {
+			if (Object.hasOwnProperty.call(block.oStrs, fieldKey)) {
+				if (fields.includes(fieldKey)) {
+					curFieldsLength++;
+				}
+			}
+		}
+  }
+
+	export let expaned = false;
+  function focused(e) {
+		e.target.select();
+		expaned = true;
+	}
+	function blured() {
+		expaned = false;
+	}
+
+  let editing;
+  function keydown(e) {
+		if (isFirstBlock && fieldId === 0 && e.key === 'Tab' && e.shiftKey) {
+			e.preventDefault();
+			e.stopPropagation();
+			dispatch('pageInc', -1);
+		}
+		if (isLastBlock && fieldId === curFieldsLength - 1 && e.key === 'Tab' && !e.shiftKey) {
+			e.preventDefault();
+			e.stopPropagation();
+			dispatch('pageInc', 1);
+		}
+	}
+
+  let inputRef;
+  onMount(() => {
+		if (isFirstBlock && fieldId === 0) {
+			inputRef.focus();
+			inputRef.select();
+		}
+	});
+</script>
+
+<textarea
+  bind:this={inputRef}
+
+  bind:value={decodedValue}
+  on:change={decodedValueChanged}
+  on:input={decodedValueChanged}
+
+  on:input={() => (editing = true)}
+
+  on:focus={focused}
+  on:blur={blured}
+
+  on:keydown={keydown}
+
+  class="w-full h-8 p-1 ring-offset-1 ring-offset-transparent peer-checked:h-36 transition-all rounded outline-none resize-none shadow-inner peer-checked:bg-slate-900/50 text-slate-200 {editing
+    ? 'ring-2 ring-purple-800 bg-purple-900/50'
+    : 'focus:ring-sky-500 focus:ring-2 bg-slate-900/25'}"
+/>
