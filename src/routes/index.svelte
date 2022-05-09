deaa897250dc9e9bca064afbf1570cc78c24dd52
Peter Pakanun <lpeachl@gmail.com>
2022-05-10 04:10:18 +0700

    minor ui adjustment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -163,10 +163,10 @@
     <div class="flex items-center">
       {#if curMaxPage >= 1}
       <div class="flex items-center gap-3 mr-4">
+        <span>Page {curPage + 1} of {curMaxPage}</span>
         <button class="p-1 transition rounded bg-slate-900/50 hover:bg-slate-600/50" title="ALT + Left Arrow" on:click={() => pageInc({detail: -1})} tabindex="-1">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>
         </button>
-        <span>Page {curPage + 1} of {curMaxPage}</span>
         <button class="p-1 transition rounded bg-slate-900/50 hover:bg-slate-600/50" title="ALT + Right Arrow" on:click={() => pageInc({detail: 1})} tabindex="-1">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
         </button>
@@ -244,7 +244,7 @@
       {#each blocks as block, index}
         <Block {block} definition={definition} isFirstBlock={index === 0} isLastBlock={index === blocks.length - 1} on:pageInc={pageInc} />
       {:else}
-        <span class="text-2xl">No results found for query string "<strong>{curQuery}</strong>".</span>
+        <span class="text-2xl">No results found {#if curSheet !== "__all" && curSheet !== "__attention"}<span> inside "<strong>{curSheet}</strong>" </span>{/if} for query string "<strong>{curQuery}</strong>".</span>
       {/each}
     {:catch error}
       {error}
