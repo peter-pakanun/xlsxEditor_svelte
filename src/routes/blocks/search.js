a63fa11fc89bfe82f4abfe3044611cc0508a2632
Peter Pakanun <lpeachl@gmail.com>
2022-05-09 13:23:54 +0700

    search endpoint

---
+++ src/routes/blocks/search.js
@@ -0,0 +1,48 @@
+import clientPromise from '$lib/db';
+
+export async function get({ params, locals, url }) {
+  let { page } = params;
+  let { user } = locals;
+  let query = url.searchParams?.get('q');
+
+  if (!user) {
+    return {
+      status: 401,
+    };
+  }
+  if (!user.language) {
+    return {
+      status: 403,
+    };
+  }
+
+  const client = await clientPromise;
+  const Blocks = client.db().collection(user.language + '_blocks');
+
+  let searchResults = await Blocks.aggregate([
+    {
+      '$search': {
+        'index': 'default',
+        'text': {
+          'query': 'Fire',
+          'path': {
+            'wildcard': '*'
+          }
+        }
+      }
+    },
+    {
+      $limit: 10
+    },
+  ]).toArray().catch(() => { console.error('error'); });
+
+  if (!Array.isArray(searchResults)) {
+    return {
+      status: 500,
+    };
+  }
+
+  return {
+    body: searchResults,
+  }
+}
